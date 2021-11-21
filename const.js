d0d949588c57bf974916d6e04c122ab38a6e3c16
Ryujin Jakka <xbalrogx@hotmail.it>
2021-11-21 17:25:30 +0100

    Small changes
    Fixed small changes

--- const.js
+++ const.js
@@ -1,3 +1,7 @@
 const pathName = "/it/cyberspaziomaterialedidattico.aspx";
 const classSpanTime = ".vjs-remaining-time-display";
 const linkButtonID = "#ctl01_mainContent_ctl00_hlLezioneSuccessiva";
+const videoId = "#videojsplayer_html5_api";
+const prefix_log_1 = "durata (s): ";
+const prefix_log_2 = "Timeout - durata (ms): ";
+const prefix_log_3 = "Timeout - durata max (ms): ";
--- cv.js
+++ cv.js
@@ -1,19 +1,19 @@
 if( window.location.pathname == pathName )
-{	
+{
 	$(window).load(function() {
-		var spanDurata = $(classSpanTime)[0];
-		if( spanDurata !== undefined ){
-			durata = spanDurata.innerText;
-			
-			durataInMSecs = moment.duration(`00:` + durata).asMilliseconds();
+		let durata = $(videoId)[0].duration;
+		if( durata !== undefined ){
+			console.log(prefix_log_1 + durata);
+			durataInMSecs = moment.duration(`00:00:` + durata).asMilliseconds();
 			durataMaxInMSecs = moment.duration(`00:59:00`).asMilliseconds();
-
 			if(durataInMSecs < durataMaxInMSecs && durataInMSecs > 0){
+				console.log(prefix_log_2 + durataInMSecs);
 				setTimeout(function() {
 					$(linkButtonID)[0].click();
 				},durataInMSecs);
 			}
 			else{
+				console.log(prefix_log_3 + durataMaxInMSecs);
 				setTimeout(function() {
 					$(linkButtonID)[0].click();
 				},durataMaxInMSecs);
